3acbfc975a8bffddb14c672ac80ddd10dc567fe1
Enis Uzun <enisbesiktasli@gmail.com>
2026-02-24 19:49:23 +0300

    update App component to reflect project name and add age information

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ function App() {
   return (
     <div>
       <h1>Web Tasarimi ve Programlama</h1>
-      <h2>LAB-1</h2>
+      <h2>webProje</h2>
+      <p>Yaş: 20</p>
       <p>Ad Soyad: Enis Uzun</p>
       <p>Ogrenci No: 230541106</p>
       <p>Hobiler: Oyun oynamak</p>
